07c621fbee4a8b560cdfb5a4bc3231bec156d579
William <will@claritycyber.com>
2022-09-17 19:14:06 -0400

    Character can no longer multi jump Implemented Collision with Mojo Jojo Game over text

--- PyGame.py
+++ PyGame.py
@@ -2,14 +2,16 @@ import pygame, random
 from gui import GameGUI
 from sys import exit #exits code right after it runs
 
-#starts and initiates pygames
+from pygame.examples.eventlist import font
+
+# starts and initiates pygames
 pygame.init()
 
-#Create display surface (the screen players see)
-#screen = pygame.displat.set.mode((width, height))
+# Create display surface (the screen players see)
+# screen = pygame.displat.set.mode((width, height))
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('PowerPuffs')
-clock = pygame.time.Clock() #Helps with time and controlling the frame rate
+clock = pygame.time.Clock()  # Helps with time and controlling the frame rate
 test_font = pygame.font.Font('Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('images/Sky.png').convert()
@@ -18,7 +20,7 @@ text_surface = test_font.render('PuffGirls', False, 'Black')
 
 mojo_jojo_surface = pygame.image.load('images/mojojojo.png')
 mojo_jojo_surface = pygame.transform.scale(mojo_jojo_surface, (120, 100)).convert_alpha()
-mojo_rect = mojo_jojo_surface.get_rect(bottomright = (600,310))
+mojo_rect = mojo_jojo_surface.get_rect(bottomright=(600, 310))
 
 bubbles_surface = pygame.image.load('images/blue.png')
 bubbles_surface = pygame.transform.scale(bubbles_surface, (120,120)).convert_alpha()
@@ -28,23 +30,38 @@ bubbles_velocity = 5
 
 crushed_can_surface = pygame.image.load('images/crushedCan.png').convert_alpha()
 crushed_can_surface = pygame.transform.scale(crushed_can_surface, (75, 75)).convert_alpha()
-crushed_can_rect = crushed_can_surface.get_rect(midbottom = (random.randrange(0, 800), 0))
+crushed_can_rect = crushed_can_surface.get_rect(midbottom=(random.randrange(0, 800), 0))
 crushed_can_gravity = -5
 
 recyclingbin_surface = pygame.image.load('images/recyclingbin.png').convert_alpha()
 recyclingbin_surface = pygame.transform.scale(recyclingbin_surface, (100, 100)).convert_alpha()
-recyclingbin_rect = recyclingbin_surface.get_rect(midbottom = (0, 250))
+recyclingbin_rect = recyclingbin_surface.get_rect(midbottom=(0, 250))
 
 trashcan_surface = pygame.image.load('images/trashcan.png').convert_alpha()
 trashcan_surface = pygame.transform.scale(trashcan_surface, (75, 75)).convert_alpha()
 trashcan_rect = trashcan_surface.get_rect(midbottom = (800, 250))
+recyclingbin_rect = trashcan_surface.get_rect(midbottom=(800, 250))
 
-score = 0
+# Keeps code running forever
 
 # Load and play background soundtrack
 gui = GameGUI()
 gui.start()
 gui.play_sound('game-soundtrack')
+
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+score = 0
+
+active = True
+
+
+def isCollision(x1, y1, x2, y2):
+    distance = math.sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2))
+    if distance < 27:
+        return True
+
+
 
 #Keeps code running forever
 while True:
@@ -52,13 +69,14 @@ while True:
     # Player input; Exit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit() #opposite of init(); closes pygame
+            pygame.quit()  # opposite of init(); closes pygame
             exit()
-      # if event.type == pygame.MOUSEMOTION:
+        # if event.type == pygame.MOUSEMOTION:
         # if   bubbles_rect.collidepoint(event.pos): print('collision')
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                bubbles_gravity = -20
+                if bubbles_rect.y == 195:
+                    bubbles_gravity = -20
             if event.key == pygame.K_LEFT:
                 bubbles_velocity = bubbles_velocity - 5
             if event.key == pygame.K_RIGHT:
@@ -72,22 +90,24 @@ while True:
     # places image ontop of display surface
     # anything in this while loop will be displayed to the player
     screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0,300))
+    screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
     screen.blit(recyclingbin_surface, (0, 230))
     screen.blit(trashcan_surface, (700, 230))
+
+
 
     mojo_rect.x -= 2
     if mojo_rect.right <= 0: mojo_rect.left = 800
     screen.blit(mojo_jojo_surface, mojo_rect)
 
-    #crushed can
+    # crushed can
     crushed_can_rect.y += 2
-    if crushed_can_rect.y > 300:
+    if crushed_can_rect.y > 240:
         crushed_can_rect = crushed_can_surface.get_rect(midbottom=(random.randrange(0, 800), 0))
     screen.blit(crushed_can_surface, crushed_can_rect)
 
-   #bubbles
+    # bubbles
     bubbles_gravity += 1
     bubbles_rect.y += bubbles_gravity
     bubbles_rect.x += bubbles_velocity
@@ -102,7 +122,23 @@ while True:
         print('collision')
 
     if bubbles_rect.colliderect(crushed_can_rect):
-        score += 1;
+        score += 1
 
+    if (isCollision(bubbles_rect.x, bubbles_rect.y, mojo_rect.x, mojo_rect.y)):
+        active = False
+        clock.tick(0)
+        break
+
+    if(active):
+        pygame.display.update()
+        clock.tick(60)  # tells the while loop to not run faster than 60
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # opposite of init(); closes pygame
+            exit()
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 200))
     pygame.display.update()
-    clock.tick(60) #tells the while loop to not run faster than 60
+    clock.tick(60)
