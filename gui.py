697d07d5b2f0b30a1334901060a28f443bf57664
Emilie Z <emilieyzh@gmail.com>
2022-09-18 00:25:53 -0400

    Lowered Game volume across all sound effects

--- gui.py
+++ gui.py
@@ -19,6 +19,7 @@ class GameGUI:
         self.sounds = {}
         for sound_file in glob.glob(f".{DIR_CHAR}sounds{DIR_CHAR}*.mp3"):
             sound = pygame.mixer.Sound(sound_file)
+            sound.set_volume(0.1)
             self.sounds[sound_file] = sound
 
 
